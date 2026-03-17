69ce87ce7367947d07fe39176ca324fe43fabae1
Julian Vargas <julianvarse2006@gmail.com>
2026-03-16 20:14:21 -0500

    Delete ecommerceusb directory

--- ecommerceusb/repository/InventoryRepository.java
+++
@@ -1,7 +0,0 @@
-package co.edu.usbcali.ecommerceusb.repository;
-
-import co.edu.usbcali.ecommerceusb.model.Inventory;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface InventoryRepository extends JpaRepository<Inventory, Integer> {
-}
--- ecommerceusb/repository/ProductCategoryRepository.java
+++
@@ -1,7 +0,0 @@
-package co.edu.usbcali.ecommerceusb.repository;
-
-import co.edu.usbcali.ecommerceusb.model.ProductCategory;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductCategoryRepository extends JpaRepository<ProductCategory, Integer> {
-}
