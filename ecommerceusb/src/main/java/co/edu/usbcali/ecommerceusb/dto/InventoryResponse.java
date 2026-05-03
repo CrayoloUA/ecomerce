4e3656a43817092f965fce641297248359ef5783
Julian Vargas <julianvarse2006@gmail.com>
2026-05-03 12:48:58 -0500

    feat: add Payment, Inventory, InventoryMovement, ProductCategory complete CRUD

---
+++ ecommerceusb/src/main/java/co/edu/usbcali/ecommerceusb/dto/InventoryResponse.java
@@ -0,0 +1,19 @@
+package co.edu.usbcali.ecommerceusb.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.OffsetDateTime;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class InventoryResponse {
+    private Integer id;
+    private Integer productId;
+    private Integer stock;
+    private OffsetDateTime updatedAt;
+}
