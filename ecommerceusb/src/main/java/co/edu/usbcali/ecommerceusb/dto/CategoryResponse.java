a16ca4fef5a58fbe1737a3d509abab3f5a2a7273
Julian Vargas <julianvarse2006@gmail.com>
2026-05-03 12:44:20 -0500

    feat: add Category complete CRUD (DTO, Service, Impl, Controller)

---
+++ ecommerceusb/src/main/java/co/edu/usbcali/ecommerceusb/dto/CategoryResponse.java
@@ -0,0 +1,19 @@
+package co.edu.usbcali.ecommerceusb.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.OffsetDateTime;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class CategoryResponse {
+    private Integer id;
+    private String name;
+    private Integer parentId;
+    private OffsetDateTime createdAt;
+}
