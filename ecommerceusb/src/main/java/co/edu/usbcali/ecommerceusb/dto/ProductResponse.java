57ec0a82436456af4a71430751e4fadb9732ce6b
Julian Vargas <julianvarse2006@gmail.com>
2026-05-03 12:46:07 -0500

    feat: add Product complete CRUD (DTO, Service, Impl, Controller)

---
+++ ecommerceusb/src/main/java/co/edu/usbcali/ecommerceusb/dto/ProductResponse.java
@@ -0,0 +1,23 @@
+package co.edu.usbcali.ecommerceusb.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.OffsetDateTime;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class ProductResponse {
+    private Integer id;
+    private String name;
+    private String description;
+    private BigDecimal price;
+    private Boolean available;
+    private OffsetDateTime createdAt;
+    private OffsetDateTime updatedAt;
+}
